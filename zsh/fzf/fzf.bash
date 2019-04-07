34b1fcf5a6e599ed5ac520f7c2a530be74d07f02
felipefacundes <felipe.facundes@gmail.com>
2019-04-07 19:16:06 -0300

    zsh - personalização com previsão de comandos
    zsh - personalização com previsão de comandos - Seja mais produtivo, com velocidade.

--- zsh/fzf/fzf.bash
+++ zsh/fzf/fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/$USER/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/rfacundes/.fzf/bin"
+  export PATH="${PATH:+${PATH}:}/home/$USER/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/rfacundes/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/$USER/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/rfacundes/.fzf/shell/key-bindings.bash"
+source "/home/$USER/.fzf/shell/key-bindings.bash"
