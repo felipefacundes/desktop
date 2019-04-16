8048cc983a8b7b6ffcb27f7d9c2161c96ea7c640
felipefacundes <felipe.facundes@gmail.com>
2019-04-16 01:44:02 -0300

    Delete bw.sh

--- scripts/bw.sh
+++
@@ -1,9 +0,0 @@
-convert *.pgm -depth 8 \
--colorspace Gray -type grayscale \
--channel rgba -alpha set -fuzz 50% \
--fill none -opaque white \
--fill white -opaque black \
--fill black -opaque none \
--colors 2 -strip \
--alpha off -negate -threshold 1% -density 300x300 imagem.tif
-#-fill white -colorize 5 imagem.tif
