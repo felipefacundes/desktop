24206015e15e2206b6483c0cd699a00c01e6028e
felipefacundes <felipe.facundes@gmail.com>
2019-05-04 20:16:44 -0300

    Scripts do Wine - para iniciar sem precisar de PlayOnLinux
    Scripts do Wine - para iniciar sem precisar de PlayOnLinux

--- wine-jogos/lol.sh
+++ wine-jogos/lol.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
-# Para instalar a droga do LOL
-# Tutorial fonte: https://appdb.winehq.org/objectManager.php?sClass=version&iId=36323&iTestingId=104834
-#
+
 #cd ~/.local/share/applications
 #rm -rf wine*
-#rm mimeinfo.cache
-cd ~
-rm -rf .wine
 export TERM=xterm
 #export GALLIUM_HUD="cpu,fps"
+#export PATH=/bin/
 export WINE=/usr/bin/wine
-#export PATH=/usr/bin/
 export WINEVERPATH=/usr/bin/wine
 export WINESERVER=/usr/bin/wineserver
 export WINELOADER=/usr/bin/wine
 #export WINEDLLPATH=
-export LD_LIBRARY32_PATH=/usr/lib
-export LD_LIBRARY64_PATH=/usr/lib32
+export LD_LIBRARY32_PATH=/usr/bin/lib32
+export LD_LIBRARY64_PATH=/usr/bin/lib
 #export WINEDEBUG=+fps
-export WINEARCH=win32
-export WINEPREFIX=~/.wine
+export WINEPREFIX=~/.age_of_empires_III/
+# Esta é uma opção que às vezes é necessária para alguns jogos
+#MESA_GL_VERSION_OVERRIDE=4.1 MESA_GLSL_VERSION_OVERRIDE=410 DRI_PRIME=1
+# Para tornar a prefix do wine preparada para 32bis. Opção necessária para alguns jogos:
+#export WINEARCH=win32
+export vblank_mode=0
+export DRI_PRIME=1
+
+# Aqui prepara o Wine para o jogo poder rodar:
 /usr/bin/winetricks corefonts vcrun2017 d3dx9
 /usr/bin/winetricks winetricks glsl=disabled
-sleep 5
-/usr/bin/winecfg
-# Essa opção será depois de tudo instalado você irá indicar o binário (.exe) do LoL, da pasta Programs ou Arquivos de Programas
-# Aí é só alterar e descomentar
+# Aqui é o caminho do jogo:
+cd ""
 
-#cd "diretório do .exe"
-#export MESA_GLTHREAD=true __GL_THREADED_OPTIMIZATIONS=0 Arquivo.exe
+/usr/bin/wine Setup.exe
+
+# Opções extras:
+# programa.exe -opengl
+# progrma.exe -dx9
