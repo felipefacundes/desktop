092622429de3d478850c90fc08cf76c55338b2bb
felipefacundes <felipe.facundes@gmail.com>
2019-05-04 20:33:55 -0300

    Scripts do Wine - para iniciar sem precisar de PlayOnLinux
    Scripts do Wine - para iniciar sem precisar de PlayOnLinux

--- wine-jogos/lol.sh
+++ wine-jogos/lol.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
+########### Este script irá usar o wine do sistema. Mas, você poderá usar um wine na versão e local de sua escolha
 #cd ~/.local/share/applications
 #rm -rf wine*
 export TERM=xterm
 #export GALLIUM_HUD="cpu,fps"
-#export PATH=/bin/
 export WINE=/usr/bin/wine
 export WINEVERPATH=/usr/bin/wine
 export WINESERVER=/usr/bin/wineserver
@@ -13,10 +12,11 @@ export WINELOADER=/usr/bin/wine
 export LD_LIBRARY32_PATH=/usr/bin/lib32
 export LD_LIBRARY64_PATH=/usr/bin/lib
 #export WINEDEBUG=+fps
+# Prefix do wine, destino do prefix individual para cada jogo é melhor e evita futuras falhas
 export WINEPREFIX=~/.LoL/
 # Esta é uma opção que às vezes é necessária para alguns jogos
 #MESA_GL_VERSION_OVERRIDE=4.1 MESA_GLSL_VERSION_OVERRIDE=410 DRI_PRIME=1
-# Para tornar a prefix do wine preparada para 32bis. Opção necessária para alguns jogos:
+# Para tornar a prefix do wine preparada para 32bits. Opção necessária para alguns jogos:
 #export WINEARCH=win32
 export vblank_mode=0
 export DRI_PRIME=1
@@ -27,7 +27,10 @@ export DRI_PRIME=1
 # Aqui é o caminho do jogo:
 cd ""
 
-/usr/bin/wine Setup.exe
+# Primeiro configurar o wine
+/usr/bin/winecfg
+# Depois executar o instalador e depois o jogo
+#/usr/bin/wine Setup.exe
 
 # Opções extras:
 # programa.exe -opengl
