e5b4a0da0f5467ca83372bd1a51c433e935413a2
felipefacundes <felipe.facundes@gmail.com>
2019-05-04 23:32:14 -0300

    Scripts do Wine - para iniciar sem precisar de PlayOnLinux
    Scripts do Wine - para iniciar sem precisar de PlayOnLinux

--- wine-jogos/ageofempiresIII.sh
+++ wine-jogos/ageofempiresIII.sh
@@ -21,16 +21,23 @@ export WINEPREFIX=~/.age_of_empires_III/
 export vblank_mode=0
 export DRI_PRIME=1
 
-# Aqui prepara o Wine para o jogo poder rodar:
-/usr/bin/winetricks corefonts d3dx9 quartz mfc42 l3codecx msxml4 vcrun6 riched20
-# Aqui é o caminho do jogo:
-cd ""
+# Aqui prepara o Wine para o jogo poder rodar:         #l3codecx
+/usr/bin/winetricks -q corefonts d3dx9 quartz mfc42 msxml4 vcrun6 riched20 wmp9 xact
+# Aqui é o caminho do jogo e pode ser alterado por você, de acordo com às suas necessidades:
+cd "/home/maria/.age_of_empires_III/drive_c/Program Files (x86)/Microsoft Games/Age of Empires III"
 
 # Primeiro configurar o wine
-/usr/bin/winecfg
+#/usr/bin/winecfg
 # Depois executar o instalador e depois o jogo
-#/usr/bin/wine Setup.exe
+#/usr/bin/wine regsvr32 l3codecx.ax
+/usr/bin/wine age3.exe
 
 # Opções extras:
 # programa.exe -opengl
 # progrma.exe -dx9
+# Nosso grupo do Telegram para ajudar o pessoal a rodar jogos do Windows no Linux. E para fazer pedidos de tutorial:
+# https://t.me/winehq_linux
+# Canal do telegram. Para os arquivos: mfc42.dll e l3codecx.ax só efetuar a busca com o hashtag: #ageofempires
+# https://t.me/archLinux_pacotes
+# Tutorial do wine, porém fiz alterações para funcionar corretamente:
+# https://appdb.winehq.org/objectManager.php?sClass=version&iId=29761&iTestingId=82749
