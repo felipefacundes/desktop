df28f76ae6d508f9a30626be66649f5b7b7a66af
felipefacundes <felipe.facundes@gmail.com>
2019-05-06 23:34:43 -0300

    Scripts do Wine - para iniciar sem precisar de PlayOnLinux
    Scripts do Wine - para iniciar sem precisar de PlayOnLinux

--- wine-jogos/ageofempiresIII.sh
+++ wine-jogos/ageofempiresIII.sh
@@ -48,3 +48,51 @@ cd "/home/$USER/.age_of_empires_III/drive_c/Program Files/Microsoft Games/Age of
 # https://www.dlldump.com/download-dll-files_new.php/dllfiles/M/mfc42.dll/6.0.400/download.html
 # Codecs:  Baixe a versão Mega:  K-Lite_Codec_Pack_1494_Mega.exe  # Depois tem que configurar: às preferencias de codec e habilitar tudo.
 # https://www.codecguide.com/download_kl.htm
+
+#
+#                    ,cldxOxoc:;,
+#               ,;:okKNXKK0kO0Okxddol:;,
+#        ,;codxkkOKXKko:'......,;clx0KXXOxol:,
+#   :lodxxdololc:,'..................,cdk00kxkOkxoc,
+#  ;XOxdl:,..  .......................... ..,;lx0XNx
+#  lX0c      ..;dddddooooollll,............     .0XO'
+#  oKK;    ....0NNXXKKK00OOOkx'.............     x0x,
+#  lXX;   ....,WWNNNKdddoooooc.'..............   okk,
+#  cXW:  .....xWWWWWl''''''''''''''............  dd0,
+#  :OWo ......NMMMMWOkkkkxxxc''''''''.......... .kxK'
+#  ,dKk .....lWWWWMMMWWWNNNX:,''''''''...........KOx'
+#  ,lk0......ONNNWWddddddddo,,,,,''''''.........lNOc'
+#  'cdk;....;KXXXNO''',,,,,,,ddddoooolllccc:....0WO,'
+#  ',ddd....o0KKKXl'',,,,,,,lWWNNNXXKKK00OOc...,0Xx'
+#   'llo;...kO00K0,'',,,,,,,0MWWWN0OOOkkkxx'...oxK;'
+#   ',xcl..,lloool''',,,,,,:WWMMMX''''''......cxko'
+#    ':x:c......''''''',,,,xNWWWM0dddddool...:00O,'
+#     'ld::.......'''''',,,KNNNWWWMMMWWWWx..;XXK;'
+#      'dd::.......'''''''lKXXNN0kkOOOkkk;.;0KK;'
+#       'ox::........'''''x0KKKX;'''......lK0k;'
+#        'cx:c,........'.,OO00KO........'kN0o,'
+#         ',dllc'........lkkOO0c.......:KNk:'
+#           ':lldc.......oodddd'.....,d0Oc,'
+#            ',:ldxl'..............'lxxo;,'
+#              ',;lx0x;..........,cldo:'
+#               '',cxXXd,....,cdxkd:'
+#                   ',;o0N0odkkkk,'
+#                     ',;lxdl:,'
+#
+# Tutorial by:
+#  ___    _             ___                     _
+# | __|__| (_)_ __  ___| __|_ _ __ _  _ _ _  __| |___ ___
+# | _/ -_) | | '_ \/ -_) _/ _` / _| || | ' \/ _` / -_|_-<
+# |_|\___|_|_| .__/\___|_|\__,_\__|\_,_|_||_\__,_\___/__/
+#            |_|
+#
+# ┏┓
+# ┃┃╱╲ nesta
+# ┃╱╱╲╲ casa
+# ╱╱╭╮╲╲ todos
+# ▔▏┗┛▕▔ usam
+# ╱▔▔▔▔▔▔▔▔▔▔╲
+# LINUX
+# ╱╱┏┳┓╭╮┏┳┓ ╲╲
+# ▔▏┗┻┛┃┃┗┻┛▕▔
+# --------------------------
