3b41aca8fee92ea5deb750d57abe5e1b92d80c96
felipefacundes <felipe.facundes@gmail.com>
2019-05-28 19:04:53 -0300

    Iniciar sem precisar de PlayOnLinux, Proton ou Lutris, inicie direto do menu iniciar com um desempenho muito melhor e superior.
    Iniciar sem precisar de PlayOnLinux, Proton ou Lutris, inicie direto do menu iniciar com um desempenho muito melhor e superior.

--- wine-jogos/GTAV.sh
+++ wine-jogos/GTAV.sh
@@ -13,75 +13,73 @@ mkdir -p ~/.jogos/icons/
 mkdir -p ~/.jogos/libraries/dxvk/
 mkdir -p ~/.jogos/scripts/run/
 mkdir -p ~/.jogos/setups/
-#mkdir -p ~/.jogos/wineprefixes/TheSims4/
+#mkdir -p ~/.jogos/wineprefixes/GTAV/
 
 cd ~/.jogos/scripts/run/
-wget -nc https://raw.githubusercontent.com/felipefacundes/desktop/master/wine-jogos/runs/TheSims4-run.sh
-chmod +x TheSims4-run.sh
+wget -nc https://raw.githubusercontent.com/felipefacundes/desktop/master/wine-jogos/runs/GTAV-run.sh
+chmod +x GTAV-run.sh
 cd ~/.jogos/icons/
-wget -nc https://raw.githubusercontent.com/felipefacundes/desktop/master/wine-jogos/icons/TheSims4.png
+wget -nc https://raw.githubusercontent.com/felipefacundes/desktop/master/wine-jogos/icons/GTAV.png
 cd ~/.jogos/scripts/
 wget -nc https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
 chmod +x winetricks
 cd ~/.jogos/wines/
-rm -rf wine-staging-4.9-1-x86_64
-# https://lutris.nyc3.digitaloceanspaces.com/runners/wine/wine-tkg-4.6-x86_64.tar.gz
-wget -c https://www.opencode.net/felipefacundes/wine-bins/raw/master/wine-staging-4.9-1-x86_64.tar.xz
-tar -xf wine-staging-4.9-1-x86_64.tar.xz
+rm -rf wine-staging-4.8-1-x86_64
+wget -nc https://www.opencode.net/felipefacundes/wine-bins/raw/master/wine-staging-4.8-1-x86_64.tar.xz
+tar -xf wine-staging-4.8-1-x86_64.tar.xz
 
 # Criando o atalho .desktop
 cd ~/.local/share/applications/
-echo "#!/usr/bin/env xdg-open" > TheSims4.desktop
-echo "[Desktop Entry]" >> TheSims4.desktop
-echo "Name=The Sims 4" >> TheSims4.desktop
-echo "Comment=Jogo ao estilo Second Life (brincar de "casinha" kkk)" >> TheSims4.desktop
-echo "Categories=Game;" >> TheSims4.desktop
-echo "Exec=/home/$USER/.jogos/scripts/run/TheSims4-run.sh" >> TheSims4.desktop
-echo "Type=Application" >> TheSims4.desktop
-echo "StartupNotify=true" >> TheSims4.desktop
-echo "Icon=/home/$USER/.jogos/icons/TheSims4.png" >> TheSims4.desktop
-echo "Terminal=false" >> TheSims4.desktop
+echo "#!/usr/bin/env xdg-open" > GTAV.desktop
+echo "[Desktop Entry]" >> GTAV.desktop
+echo "Name=GTA V" >> GTAV.desktop
+echo "Comment=Jogo de Ação" >> GTAV.desktop
+echo "Categories=Game;" >> GTAV.desktop
+echo "Exec=/home/$USER/.jogos/scripts/run/GTAV-run.sh" >> GTAV.desktop
+echo "Type=Application" >> GTAV.desktop
+echo "StartupNotify=true" >> GTAV.desktop
+echo "Icon=/home/$USER/.jogos/icons/GTAV.png" >> GTAV.desktop
+echo "Terminal=false" >> GTAV.desktop
 
 # Essa é a versão escolhida do Wine
 export TERM=xterm
 # Para ver o FPS na tela, para CPU, inclua cpu,fps
 #export GALLIUM_HUD="fps"
 #export WINE=~/.jogos/wines/Proton-4.6-GE-2/dist/bin/wine
-export WINE=~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine
+export WINE=~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine
 #export WINEVERPATH=~/.jogos/wines/Proton-4.6-GE-2/dist/bin/wine
-export WINEVERPATH=~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine
+export WINEVERPATH=~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine
 #export WINESERVER=~/.jogos/wines/Proton-4.6-GE-2/dist/bin/wineserver
-export WINESERVER=~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wineserver
+export WINESERVER=~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wineserver
 #export WINELOADER=~/.jogos/wines/Proton-4.6-GE-2/dist/bin/wine
-export WINELOADER=~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine
-#export WINEDLLPATH=$WINEPATH”~/.jogos/wines/wine-staging-4.9-1-x86_64/lib/wine/fakedlls”
+export WINELOADER=~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine
+#export WINEDLLPATH=$WINEPATH”~/.jogos/wines/wine-staging-4.8-1-x86_64/lib/wine/fakedlls”
 #export LD_LIBRARY32_PATH=~/.jogos/wines/Proton-4.6-GE-2/dist/lib/
-export LD_LIBRARY32_PATH=~/.jogos/wines/wine-staging-4.9-1-x86_64/lib32/
+export LD_LIBRARY32_PATH=~/.jogos/wines/wine-staging-4.8-1-x86_64/lib32/
 #export LD_LIBRARY64_PATH=~/.jogos/wines/Proton-4.6-GE-2/dist/lib64/
-export LD_LIBRARY64_PATH=~/.jogos/wines/wine-staging-4.9-1-x86_64/lib/
+export LD_LIBRARY64_PATH=~/.jogos/wines/wine-staging-4.8-1-x86_64/lib/
 
 export WINEDEBUG=-all
 # Prefix do wine, destino do prefix individual para cada jogo é melhor e evita futuras falhas
-export WINEPREFIX=~/.jogos/wineprefixes/TheSims4
+export WINEPREFIX=~/.jogos/wineprefixes/GTAV
 # Esta é uma opção que às vezes é necessária para alguns jogos
 #MESA_GL_VERSION_OVERRIDE=4.1 MESA_GLSL_VERSION_OVERRIDE=410 DRI_PRIME=1
 # Para tornar a prefix do wine preparada para 32bits ou 64bits. Opção necessária para alguns jogos:
 export WINEARCH=win64
-export WINEESYNC=0
+export WINEESYNC=1
 export vblank_mode=0
 export DRI_PRIME=1
-#export DXVK_HUD=1
+export DXVK_HUD=1
 
 # Aqui prepara o Wine para o jogo poder rodar:        # Não use -> l3codecx     # Opção para winetricks: dlls list
-~/.jogos/scripts/winetricks -q corefonts vlc d3dx9 d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 gdiplus vulkansdk
-#~/.jogos/scripts/winetricks -q vcrun2008 vcrun2010 vcrun2013 vcrun2015 vcrun2017
+~/.jogos/scripts/winetricks -q corefonts vlc d3dx9 vcrun2012 xact d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 gdiplus vulkansdk steam
 
 # Faça uma instalação manual do dxsdk_jun2010    # https://www.microsoft.com/en-us/download/details.aspx?id=6812
 # vamos instalar o DXSDK
 mkdir -p ~/.jogos/setups/DXSDK_Jun10/
 cd ~/.jogos/setups/DXSDK_Jun10/
 #wget -nc https://download.microsoft.com/download/A/E/7/AE743F1F-632B-4809-87A9-AA1BB3458E31/DXSDK_Jun10.exe -O DXSDK_Jun10.exe
-#~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine DXSDK_Jun10.exe
+#~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine DXSDK_Jun10.exe
 
 # Para DXVK - SOMENTE IRÁ FUNCIONAR SE O VULKAN DA SUA PLACA ESTIVER HABILITADO
 cd ~/.jogos/libraries/dxvk/
@@ -94,25 +92,19 @@ bash ~/.jogos/libraries/dxvk/d9vk-0.11/setup_dxvk.sh install
 bash ~/.jogos/libraries/dxvk/dxvk-1.2.1/setup_dxvk.sh install
 
 # Executar o instalador e depois o jogo
-~/.jogos/scripts/winetricks -q win7
 
+~/.jogos/scripts/winetricks -q win7
 # Primeiro configurar o wine
-~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/winecfg
+~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/winecfg
 
 # Aqui é o caminho do jogo e pode ser alterado por você, de acordo com às suas necessidades:
-cd ~/.jogos/setups/
-#wget -nc https://origin-a.akamaihd.net/Origin-Client-Download/origin/live/OriginThinSetup.exe
-rm -rf OriginThinSetup.exe
-wget https://origin-a.akamaihd.net/Origin-Client-Download/origin/legacy/OriginThinSetup.exe
-~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine OriginThinSetup.exe
-
-cd "/home/$USER/.jogos/wineprefixes/TheSims4/drive_c"
-#~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine Steam.exe
+cd "/home/$USER/.jogos/wineprefixes/GTAV/drive_c/Program Files (x86)/Steam/"
+~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine Steam.exe
 
 
 # Irá abrir a localização e o script de inicialização do jogo:
-#xdg-open ~/.jogos/wineprefixes/TheSims4/drive_c/ 
-#xdg-open ~/.jogos/scripts/run/TheSims4-run.sh
+xdg-open ~/.jogos/wineprefixes/GTAV/drive_c/ 
+xdg-open ~/.jogos/scripts/run/GTAV-run.sh
 
 # Opções extras:
 # programa.exe -opengl
@@ -187,17 +179,17 @@ cd "/home/$USER/.jogos/wineprefixes/TheSims4/drive_c"
 #cd ~/.local/share/applications
 #rm -rf wine*
 
-#cd ~/.jogos/wineprefixes/TheSims4/drive_c/windows/system32/
+#cd ~/.jogos/wineprefixes/GTAV/drive_c/windows/system32/
 #rm ntdll.dll
 #wget -nc https://www.dlldump.com/dllfiles/N/ntdll.dll
-#~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine regsvr32 ntdll.dll
+#~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine regsvr32 ntdll.dll
 
-#cp -rf ~/.jogos/libraries/dxvk/dxvk-1.2.1/x64/* ~/.jogos/wineprefixes/TheSims4/drive_c/windows/syswow64/
-#cp -rf ~/.jogos/libraries/dxvk/dxvk-1.2.1/x32/* ~/.jogos/wineprefixes/TheSims4/drive_c/windows/system32/
+#cp -rf ~/.jogos/libraries/dxvk/dxvk-1.2.1/x64/* ~/.jogos/wineprefixes/GTAV/drive_c/windows/syswow64/
+#cp -rf ~/.jogos/libraries/dxvk/dxvk-1.2.1/x32/* ~/.jogos/wineprefixes/GTAV/drive_c/windows/system32/
 
-#~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine regsvr32 l3codecx.ax
-#~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine regsvr32.exe /n /i d3d10.dll
-#~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine regsvr32.exe /n /i d3d10_1.dll
-#~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine regsvr32.exe /n /i d3d10core.dll
-#~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine regsvr32.exe /n /i d3d11.dll
-#~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine regsvr32.exe /n /i dxgi.dll
+#~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine regsvr32 l3codecx.ax
+#~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine regsvr32.exe /n /i d3d10.dll
+#~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine regsvr32.exe /n /i d3d10_1.dll
+#~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine regsvr32.exe /n /i d3d10core.dll
+#~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine regsvr32.exe /n /i d3d11.dll
+#~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine regsvr32.exe /n /i dxgi.dll
