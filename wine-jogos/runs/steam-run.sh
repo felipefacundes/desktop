5a0b4548bae449f5831b80db13104b658c8135bd
felipefacundes <felipe.facundes@gmail.com>
2019-05-30 15:38:29 -0300

    Iniciar sem precisar de PlayOnLinux, Proton ou Lutris, inicie direto do menu iniciar com um desempenho muito melhor e superior.
    Iniciar sem precisar de PlayOnLinux, Proton ou Lutris, inicie direto do menu iniciar com um desempenho muito melhor e superior.

--- wine-jogos/runs/steam-run.sh
+++ wine-jogos/runs/steam-run.sh
@@ -40,10 +40,27 @@ export DXVK_HUD=1
 # Para configurar o Wine descomente, (retire o hashtag #)
 # ~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/winecfg
 
-# Aqui é o caminho do jogo e pode ser alterado por você, de acordo com às suas necessidades:
+##############################################################################################
+# _          _                     _ _       _                       _           _
+#| |        (_)                   | (_)     | |                     | |         (_)
+#| |     ___ _  __ _    __ _ ___  | |_ _ __ | |__   __ _ ___    __ _| |__   __ _ ___  _____
+#| |    / _ \ |/ _` |  / _` / __| | | | '_ \| '_ \ / _` / __|  / _` | '_ \ / _` | \ \/ / _ \
+#| |___|  __/ | (_| | | (_| \__ \ | | | | | | | | | (_| \__ \ | (_| | |_) | (_| | |>  < (_) |
+#\_____/\___|_|\__,_|  \__,_|___/ |_|_|_| |_|_| |_|\__,_|___/  \__,_|_.__/ \__,_|_/_/\_\___/
+#
+# Logo abaixo é o caminho do instalador do jogo e pode ser alterado por você, de acordo com às suas necessidades:
+# Coloque o endereço da PASTA entre às "aspas" e mude o nome do executável do instalador.
+# Respeitando as Letras MAÍUSCULAS e minúsculas. Exemplo: Setup.exe
 cd "/home/$USER/.jogos/wineprefixes/steam/drive_c/Program Files (x86)/Steam/"
 ~/.jogos/wines/wine-staging-4.8-1-x86_64/bin/wine Steam.exe
+# Só altere essas duas linhas acima, como já explicado.
 
+
+
+
+
+
+# Não mexa nas demais linhas, deixa do jeito que está.
 
 # Irá abrir a localização e o script de inicialização do jogo:
 #xdg-open ~/.jogos/wineprefixes/steam/drive_c/
