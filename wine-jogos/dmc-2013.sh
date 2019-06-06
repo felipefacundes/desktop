2da1853d820bb829383b6dceec03e393762ca224
felipefacundes <felipe.facundes@gmail.com>
2019-06-06 19:18:42 -0300

    Inicie seus Jogos direto do menu iniciar, sem precisar de PlayOnLinux, Proton ou Lutris, e com um desempenho muito melhor e superior.
    Inicie seus Jogos direto do menu iniciar, sem precisar de PlayOnLinux, Proton ou Lutris, e com um desempenho muito melhor e superior.

--- wine-jogos/dmc-2013.sh
+++ wine-jogos/dmc-2013.sh
@@ -82,7 +82,7 @@ export DXVK_HUD=1
 # ⛁ Observação: vcrun2015 CONFLITA com vcrun2017
 # Aqui prepara o Wine para o jogo poder rodar:      # Não use -> l3codecx     # Opção para winetricks: dlls list   settings list
 ~/.jogos/scripts/winetricks -q corefonts mfc42 vcrun6 d3dx9 xact d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 dotnet40
-#~/.jogos/scripts/winetricks -q vcrun2005
+~/.jogos/scripts/winetricks -q vcrun2005
 ~/.jogos/scripts/winetricks -q vcrun2008
 ~/.jogos/scripts/winetricks -q vcrun2010
 #~/.jogos/scripts/winetricks -q vcrun2012
