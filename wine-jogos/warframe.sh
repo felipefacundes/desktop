50686a08c22de829a6badc75f8ba9df5ded33166
Felipe Facundes <felipe.facundes@gmail.com>
2019-06-17 18:55:47 -0300

    Update warframe.sh

--- wine-jogos/warframe.sh
+++ wine-jogos/warframe.sh
@@ -52,18 +52,21 @@ echo "Terminal=false" >> $GN.desktop
 # Essa é a versão escolhida do Wine
 export TERM=xterm
 # Para ver o FPS na tela, para CPU, inclua cpu,fps
-#export GALLIUM_HUD=".w256.h64.x1600.y520.d.c1000fps+cpu,.datom-count"
 #export GALLIUM_HUD="fps"
 W=~/.jogos/wines/$WV
+export WINE64=$W/bin/wine64
 export WINE=$W/bin/wine
 export WINEVERPATH=$W
 export WINEPATH=$W/bin:$WINEPATH
 export WINESERVER=$W/bin/wineserver
+export WINELOADER64=$W/bin/wine64
 export WINELOADER=$W/bin/wine
+export WINEDLLPATH32=$W/lib32/wine/fakedlls
+export WINEDLLPATH64=$W/lib/wine/fakedlls
 export WINEDLLPATH=$W/lib/wine/fakedlls
-export LD_LIBRARY_PATH="$W/lib:$LD_LIBRARY_PATH"
 export LD_LIBRARY32_PATH="$W/lib32:$LD_LIBRARY32_PATH"
 export LD_LIBRARY64_PATH="$W/lib:$LD_LIBRARY64_PATH"
+export LD_LIBRARY_PATH="$W/lib:$LD_LIBRARY_PATH"
 #$W/bin/wineconsole "cmd"
 
 export WINEDEBUG=-all
