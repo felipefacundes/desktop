5908e16defa9d4244bcac76a2e4d5d4872bb64d1
Felipe Facundes <felipe.facundes@gmail.com>
2019-06-17 02:40:55 -0300

    Update warframe.sh

--- wine-jogos/warframe.sh
+++ wine-jogos/warframe.sh
@@ -94,6 +94,7 @@ export DXVK_LOG_LEVEL=none
 #export PULSE_LATENCY_MSEC=60
 #export __GL_SHADER_DISK_CACHE=1
 #export __GL_SHADER_DISK_CACHE_PATH=”/tmp”
+#export __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
 export DXVK_HUD=fps
 glxinfo -B
 glxgears -stereo
