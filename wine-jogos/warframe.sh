60e18370b09bb3b85b50934d624109501d06a042
Felipe Facundes <felipe.facundes@gmail.com>
2019-06-21 02:43:25 -0300

    Update warframe.sh

--- wine-jogos/warframe.sh
+++ wine-jogos/warframe.sh
@@ -25,12 +25,12 @@ rm -rf $GN
 
 cd ~/.jogos/scripts/run/
 rm -rf $GN-run.sh
-wget -nc https://raw.githubusercontent.com/felipefacundes/desktop/master/wine-jogos/runs/$GN-run.sh > /dev/null 2>&1
+wget -nc https://raw.githubusercontent.com/felipefacundes/desktop/master/wine-jogos/runs/$GN-run.sh 
 chmod +x $GN-run.sh
 cd ~/.jogos/icons/
-wget -nc https://raw.githubusercontent.com/felipefacundes/desktop/master/wine-jogos/icons/$GN.png > /dev/null 2>&1
+wget -nc https://raw.githubusercontent.com/felipefacundes/desktop/master/wine-jogos/icons/$GN.png 
 cd ~/.jogos/scripts/
-wget -nc https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks > /dev/null 2>&1
+wget -nc https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks 
 chmod +x winetricks
 cd ~/.jogos/wines/
 rm -rf $WV
