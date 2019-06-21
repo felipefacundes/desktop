82df74a2a528c909bb84d6f8549ef421cd883493
Felipe Facundes <felipe.facundes@gmail.com>
2019-06-21 02:48:21 -0300

    Update warframe.sh

--- wine-jogos/warframe.sh
+++ wine-jogos/warframe.sh
@@ -5,7 +5,7 @@
 # Faça o seu pedido de tutorial e GamePlay no nosso:
 # 既 Grupo 調 Gamer do 切 Telegram 切: https://t.me/winehq_linux
 ########### Este script irá usar o wine personalizado. Mas, você poderá usar um wine na versão e local de sua escolha
-dialog --msgbox "A instalação poderá demorar dependendo do JOGO. Acima de tudo tenha: PACIÊNCIA. AGUARDE! Você será notificado, quando a instalação concluir." 10 30 > /dev/null 2>&1
+dialog --msgbox "A instalação poderá demorar dependendo do JOGO. Acima de tudo tenha: PACIÊNCIA. AGUARDE! Você será notificado, quando a instalação concluir." 10 30
 WV=wine-staging-4.8-1-x86_64
 GN=warframe
 SN="Warframe"
