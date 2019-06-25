244dbba70bb2666ec767a904d193dcfa3189e1a9
Felipe Facundes <felipe.facundes@gmail.com>
2019-06-25 14:58:51 -0300

    Update SFIV.sh

--- wine-jogos/SFIV.sh
+++ wine-jogos/SFIV.sh
@@ -1,14 +1,6 @@
-#!/bin/bash
+Não te interessa. O que você está olhando aqui. vai tomar conta da sua vida.
 
-export WINE=/home/maria/.jogos/wines/1.9.16/bin/wine64
-export WINEVERPATH=/home/maria/.jogos/wines/1.9.16/bin/wine64
-export LD_LIBRARY32_PATH=/home/maria/.jogos/wines/1.9.16/lib
-export LD_LIBRARY64_PATH=/home/maria/.jogos/wines/1.9.16/lib64
-
-cd "/home/maria/.jogos/wineprefixes/SSFIV/drive_c/Capcom/Super Street Fighter IV"
-#cd /home/maria/.cache/winetricks/dxsdk_jun2010
-
-WINEPREFIX=~/.jogos/wineprefixes/SSFIV /home/maria/.jogos/wines/1.9.16/bin/wine64 SSFIV.exe
+SSFIV.exe
 
 #SSFIV.exe DXSDK_Jun10.exe
 
