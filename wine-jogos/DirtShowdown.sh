c1e083e56ab06e0c004b85131ee54a3342b98349
Felipe Facundes <felipe.facundes@gmail.com>
2019-06-25 13:17:03 -0300

    Update DirtShowdown.sh

--- wine-jogos/DirtShowdown.sh
+++ wine-jogos/DirtShowdown.sh
@@ -1,6 +1,1 @@
-cd "/home/maria/.jogos/Nativos/DiRT Showdown"
-export TERM=xterm
-export GALLIUM_HUD="fps"
-#export vblank_mode=0
-#MESA_GL_VERSION_OVERRIDE=4.1 MESA_GLSL_VERSION_OVERRIDE=410 
-DRI_PRIME=1 ./dirt
+Não te interessa. O que você está olhando aqui. vai tomar conta da sua vida.
