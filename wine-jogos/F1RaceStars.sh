aa57447e5f85ce7ba354646f5b5e8e0a1670d30b
Felipe Facundes <felipe.facundes@gmail.com>
2019-06-25 13:16:14 -0300

    Update F1RaceStars.sh

--- wine-jogos/F1RaceStars.sh
+++ wine-jogos/F1RaceStars.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-#export PATH=/bin/
-#export WINE=/home/maria/.jogos/wines/3.4-staging/bin/wine64
-#export PATH=/home/maria/.jogos/wines/3.4-staging/bin/
-#export WINEVERPATH=/home/maria/.jogos/wines/3.4-staging/bin/wine64
-#export WINESERVER=/home/maria/.jogos/wines/3.4-staging/bin/wineserver
-#export WINELOADER=/home/maria/.jogos/wines/3.4-staging/bin/wine64
-#export WINEDLLPATH=
-#export LD_LIBRARY32_PATH=/home/maria/.jogos/wines/3.4-staging/lib32
-#export LD_LIBRARY64_PATH=/home/maria/.jogos/wines/3.4-staging/lib
-
-cd "/home/maria/.jogos/wineprefixes/F1RacerStars/drive_c/F1 Race Stars/"
-#cd "/home/maria/.jogos/wineprefixes/F1RacerStars/drive_c/F1 Race Stars/support/openal/"
-WINEPREFIX=~/.jogos/wineprefixes/F1RacerStars/ wine F1RaceStars.exe -opengl
+Não te interessa. O que você está olhando aqui. vai tomar conta da sua vida.
