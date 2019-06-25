d179604efa6504fa310dc279d6c5f2d976d5fbed
Felipe Facundes <felipe.facundes@gmail.com>
2019-06-25 15:03:11 -0300

    Update SSSS.sh

--- wine-jogos/SSSS.sh
+++ wine-jogos/SSSS.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
+Não te interessa. O que você está olhando aqui. vai tomar conta da sua vida.
 
-export PATH=/home/maria/.jogos/wines/2.14/bin/
-export WINE=/home/maria/.jogos/wines/2.14/bin/wine64
-export WINEVERPATH=/home/maria/.jogos/wines/2.14/bin/wine64
-export WINESERVER=/home/maria/.jogos/wines/2.14/bin/wineserver
-export WINELOADER=/home/maria/.jogos/wines/2.14/bin/wine64
-#export WINEDLLPATH=
-export LD_LIBRARY32_PATH=/home/maria/.jogos/wines/2.14/lib
-export LD_LIBRARY64_PATH=/home/maria/.jogos/wines/2.14/lib64
-
-cd "/home/maria/.jogos/wineprefixes/jgs_lutas/drive_c/Saint Seiya Soldiers Soul"
-WINEPREFIX=~/.jogos/wineprefixes/jgs_lutas /home/maria/.jogos/wines/2.14/bin/wine64 SSSS.exe
+SSSS.exe
