f771e981ba715ce12b4e369d2d71ba0465d3c969
Felipe Facundes <felipe.facundes@gmail.com>
2019-07-01 01:06:02 -0300

    Update Sekiro.sh

--- wine-jogos/Sekiro.sh
+++ wine-jogos/Sekiro.sh
@@ -130,8 +130,8 @@ echo "Em progresso ..."
 ~/.jogos/scripts/winetricks -q vcrun2012 > /dev/null 2>&1
 ~/.jogos/scripts/winetricks -q vcrun2013 > /dev/null 2>&1
 echo "Em progresso ...."
-~/.jogos/scripts/winetricks -q vcrun2015 > /dev/null 2>&1
-~/.jogos/scripts/winetricks -q vcrun2017 --force > /dev/null 2>&1
+#~/.jogos/scripts/winetricks -q vcrun2015 > /dev/null 2>&1
+~/.jogos/scripts/winetricks -q vcrun2017 > /dev/null 2>&1
 echo "Em progresso ....."
 ~/.jogos/scripts/winetricks autostart_winedbg=disable nvapi=disabled nvapi64=disabled csmt=off hosts > /dev/null 2>&1
 #~/.jogos/scripts/winetricks xaudio2_0=native xaudio2_1=native xaudio2_2=native xaudio2_3=native xaudio2_4=native xaudio2_5=native xaudio2_6=native xaudio2_7=native xaudio2_8=native xaudio2_9=native > /dev/null 2>&1
