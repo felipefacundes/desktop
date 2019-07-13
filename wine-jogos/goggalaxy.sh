a804f48cc37b06cceeed18499ca2cd4185701261
Felipe Facundes <felipe.facundes@gmail.com>
2019-07-13 18:17:55 -0300

    Update goggalaxy.sh

--- wine-jogos/goggalaxy.sh
+++ wine-jogos/goggalaxy.sh
@@ -7,7 +7,7 @@ clear -T "$TERM"
 # Faça o seu pedido de tutorial e GamePlay no nosso:
 # 既 Grupo 調 Gamer do 切 Telegram 切: https://t.me/winehq_linux
 ########### Este script irá usar o wine personalizado. Mas, você poderá usar um wine na versão e local de sua escolha
-WV=wine-staging-4.12.1-1-x86_64
+WV=wine-staging-4.11-1-x86_64
 GN=goggalaxy
 SN="GOG Galaxy"
 CME="GOG Store"
@@ -117,10 +117,10 @@ echo "TUDO dependerá do seu PROCESSADOR. Abaixo de 3GHz demorará BEM mais."
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 #~/.jogos/scripts/winetricks -q dotne462 > /dev/null 2>&1
 #~/.jogos/scripts/winetricks -q xna40 > /dev/null 2>&1
-~/.jogos/scripts/winetricks -q corefonts mfc40 mfc42 d3dx9 xact d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 gdiplus > /dev/null 2>&1
+~/.jogos/scripts/winetricks -q corefonts d3dx9 xact d3dcompiler_43 d3dcompiler_47 d3dx10 d3dx10_43 d3dx11_42 d3dx11_43 gdiplus > /dev/null 2>&1
 echo "VAMOS LÁ. VOCÊ CONSEGUE. Aguarde só MAIS UM POUCO."
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-~/.jogos/scripts/winetricks -q vcrun2005 > /dev/null 2>&1
+#~/.jogos/scripts/winetricks -q vcrun2005 > /dev/null 2>&1
 echo "Em progresso ."
 ~/.jogos/scripts/winetricks -q vcrun2008 > /dev/null 2>&1
 echo "Em progresso .."
