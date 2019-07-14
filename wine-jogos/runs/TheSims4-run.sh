100ea29eb87854cf18101c9a0b0503f89fdf7347
Felipe Facundes <felipe.facundes@gmail.com>
2019-07-14 20:36:08 -0300

    Update TheSims4-run.sh

--- wine-jogos/runs/TheSims4-run.sh
+++ wine-jogos/runs/TheSims4-run.sh
@@ -7,41 +7,70 @@
 ########### Este script irá usar o wine personalizado. Mas, você poderá usar um wine na versão e local de sua escolha
 # Criar as pastas de estrutura para o binário isolado do wine - técnica para manipular diversos tipos de wine
 
-# Essa é a versão escolhida do Wine
+#A# Essa é a versão escolhida do Wine
 export TERM=xterm
 # Para ver o FPS na tela, para CPU, inclua cpu,fps
 #export GALLIUM_HUD="fps"
-#export WINE=~/.jogos/wines/Proton-4.6-GE-2/dist/bin/wine
-export WINE=~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine
-#export WINEVERPATH=~/.jogos/wines/Proton-4.6-GE-2/dist/bin/wine
-export WINEVERPATH=~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine
-#export WINESERVER=~/.jogos/wines/Proton-4.6-GE-2/dist/bin/wineserver
-export WINESERVER=~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wineserver
-#export WINELOADER=~/.jogos/wines/Proton-4.6-GE-2/dist/bin/wine
-export WINELOADER=~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine
-#export WINEDLLPATH=$WINEPATH”~/.jogos/wines/wine-staging-4.9-1-x86_64/lib/wine/fakedlls”
-#export LD_LIBRARY32_PATH=~/.jogos/wines/Proton-4.6-GE-2/dist/lib/
-export LD_LIBRARY32_PATH=~/.jogos/wines/wine-staging-4.9-1-x86_64/lib32/
-#export LD_LIBRARY64_PATH=~/.jogos/wines/Proton-4.6-GE-2/dist/lib64/
-export LD_LIBRARY64_PATH=~/.jogos/wines/wine-staging-4.9-1-x86_64/lib/
+W=~/.jogos/wines/"$WV"
+export WINE64="$W"/bin/wine64
+export WINE="$W"/bin/wine
+export WINEVERPATH="$W"
+export WINEPATH="$W/bin:$WINEPATH"
+export WINESERVER="$W"/bin/wineserver
+export WINELOADER64="$W"/bin/wine64
+export WINELOADER="$W"/bin/wine
+export WINEDLLPATH32="$W"/lib32/wine/fakedlls
+export WINEDLLPATH64="$W"/lib/wine/fakedlls
+export WINEDLLPATH="$W"/lib/wine/fakedlls
+export LD_LIBRARY32_PATH="$W/lib32:$LD_LIBRARY32_PATH"
+export LD_LIBRARY64_PATH="$W/lib:$LD_LIBRARY64_PATH"
+export LD_LIBRARY_PATH="$W/lib:$LD_LIBRARY_PATH"
+#"$W"/bin/wineconsole "cmd"
 
 export WINEDEBUG=-all
 # Prefix do wine, destino do prefix individual para cada jogo é melhor e evita futuras falhas
-export WINEPREFIX=~/.jogos/wineprefixes/TheSims4
-# Esta é uma opção que às vezes é necessária para alguns jogos
-#MESA_GL_VERSION_OVERRIDE=4.1 MESA_GLSL_VERSION_OVERRIDE=410 DRI_PRIME=1
+export WINEPREFIX=~/.jogos/wineprefixes/"$GN"
 # Para tornar a prefix do wine preparada para 32bits ou 64bits. Opção necessária para alguns jogos:
 export WINEARCH=win64
 export WINEESYNC=0
+#export ESYNC=0
 export vblank_mode=0
+# Esta é uma opção que às vezes é necessária para alguns jogos   MESA_GL_VERSION_OVERRIDE=version
+export MESA_GLSL_VERSION_OVERRIDE=450
+export MESA_GL_VERSION_OVERRIDE=4.5COMPAT
 # Para placas gráficas híbridas use o DRI_PRIME=1
 #export DRI_PRIME=1
+export WINEDLLOVERRIDES=d3d10,d3d11,dxgi=n
+export DXVK_SPIRV_OPT=ON
+export DXVK_SHADER_OPTIMIZE=1
+export DXVK_DEBUG_LAYERS=0
+export DXVK_SHADER_DUMP_PATH="/tmp"
+export DXVK_SHADER_READ_PATH="/tmp"
 export DXVK_LOG_LEVEL=none
-export DXVK_HUD=1
+#export DXVK_HUD=fps,version,compiler
+#LD_PRELOAD=”libpthread.so.0 libGL.so.1″
+export PULSE_LATENCY_MSEC=60
+export KWIN_TRIPLE_BUFFER=1
+export TRIPLE_BUFFER=1
+export MESA_NO_ERROR=1
+export XVideoTextureSyncToVBlank=0
+export __GL_YIELD="NOTHING"
+export __GL_SYNC_TO_VBLANK=0
+export __GL_THREADED_OPTIMIZATIONS=1
+export mesa_glthread=true
+export __GL_SHADER_DISK_CACHE=1
+export __GL_SHADER_DISK_CACHE_PATH="/tmp"
+export __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
+export PBA_DISABLE=0
+export DXVK_HUD=compiler,fps
+glxinfo -B
+glxgears -stereo > /dev/null 2>&1
+
+# Para GAMEMODE: gamemoderun
+#export LD_PRELOAD="$LD_PRELOAD:/usr/\$LIB/libgamemodeauto.so.0"
 
 # Primeiro configurar o wine
-#~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/winecfg
-# Executar o instalador e depois o jogo
+#"$W"/bin/winecfg
 
 #⛔⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⧩⛔
 #  _          _                     _ _       _                       _           _
@@ -57,7 +86,6 @@ export DXVK_HUD=1
 #⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬⏬
 
 cd "/home/maria/.jogos/wineprefixes/TheSims4/drive_c/The SIMS 4/Game/Bin"
-
 ~/.jogos/wines/wine-staging-4.9-1-x86_64/bin/wine TS4_x64.exe
 
 #⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫⏫
