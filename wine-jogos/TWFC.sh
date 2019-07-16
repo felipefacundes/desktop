1b8e67b49d1e411acaf6138c5417e402b94e6a07
Felipe Facundes <felipe.facundes@gmail.com>
2019-07-16 16:19:42 -0300

    Update TWFC.sh

--- wine-jogos/TWFC.sh
+++ wine-jogos/TWFC.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-export PATH=/home/maria/.jogos/wines/2.14/bin/
-export WINE=/home/maria/.jogos/wines/2.14/bin/wine64
-export WINEVERPATH=/home/maria/.jogos/wines/2.14/bin/wine64
-export WINESERVER=/home/maria/.jogos/wines/2.14/bin/wineserver
-export WINELOADER=/home/maria/.jogos/wines/2.14/bin/wine64
-#export WINEDLLPATH=
-export LD_LIBRARY32_PATH=/home/maria/.jogos/wines/2.14/lib
-export LD_LIBRARY64_PATH=/home/maria/.jogos/wines/2.14/lib64
-
-cd "/home/maria/.jogos/wineprefixes/TWFC/drive_c/Transformers - War for Cybertron/Binaries"
-WINEPREFIX=~/.jogos/wineprefixes/TWFC/ /home/maria/.jogos/wines/2.14/bin/wine64 TWFC.exe
+Não te interessa, está tentando ver o que aqui? Vai tomar conta da sua vida.
