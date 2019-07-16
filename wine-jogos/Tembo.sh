ef1c78fb752ca15a55bc970cc02800c657dc802c
Felipe Facundes <felipe.facundes@gmail.com>
2019-07-16 16:20:59 -0300

    Update Tembo.sh

--- wine-jogos/Tembo.sh
+++ wine-jogos/Tembo.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
+Não te interessa, está tentando ver o que aqui? Vai tomar conta da sua vida.
 
-export PATH=/home/maria/.jogos/wines/2.14/bin/
-export WINE=/home/maria/.jogos/wines/2.14/bin/wine64
-export WINEVERPATH=/home/maria/.jogos/wines/2.14/bin/wine64
-export WINESERVER=/home/maria/.jogos/wines/2.14/bin/wineserver
-export WINELOADER=/home/maria/.jogos/wines/2.14/bin/wine64
-#export WINEDLLPATH=
-export LD_LIBRARY32_PATH=/home/maria/.jogos/wines/2.14/lib
-export LD_LIBRARY64_PATH=/home/maria/.jogos/wines/2.14/lib64
-
-cd "/home/maria/.jogos/wineprefixes/JogosAventura/drive_c/Games/Foxy Games/Tembo The Badass Elephant"
-WINEPREFIX=~/.jogos/wineprefixes/JogosAventura/ /home/maria/.jogos/wines/2.14/bin/wine64 "Tembo The Badass Elephant.exe"
+Old CSMT
