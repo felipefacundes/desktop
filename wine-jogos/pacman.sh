b9f029fce61a0c6f89fea329f04c34b91f1cc140
Felipe Facundes <felipe.facundes@gmail.com>
2019-07-16 14:26:58 -0300

    Update pacman.sh

--- wine-jogos/pacman.sh
+++ wine-jogos/pacman.sh
@@ -1,26 +1,9 @@
-#!/bin/bash
+Não te interessa, está tentando ver o que aqui? Vai tomar conta da sua vida.
 
-#export GALLIUM_HUD="cpu,fps;primitives-generated"
-#export PATH=/bin/
-#export PATH=/home/maria/.jogos/wines/2.18-x86/bin/
-export WINE=/home/maria/.jogos/wines/2.18-x86/bin/wine
-export WINEVERPATH=/home/maria/.jogos/wines/2.18-x86/bin/wine
-export WINESERVER=/home/maria/.jogos/wines/2.18-x86/bin/wineserver
-export WINELOADER=/home/maria/.jogos/wines/2.18-x86/bin/wine
-export WINEDLLPATH=/home/maria/.jogos/wines/2.18-x86/lib
-export LD_LIBRARY32_PATH=/home/maria/.jogos/wines/2.18-x86/lib
-export LD_LIBRARY64_PATH=/home/maria/.jogos/wines/2.18-x86/lib64
-export WINEARCH=win32
-export WINEPREFIX=~/.jogos/wineprefixes/Pacman/
-export WINEDEBUG=-all
 
-#cd "/home/maria/.jogos/wineprefixes/"Mudar o Nome"/drive_c/users/Phillipe/AppData/Roaming/Rovio/Angry Birds Star Wars/updates/Update"
-#cd "/home/maria/.jogos/wineprefixes/"Mudar o Nome"/drive_c/Games/Rovio/Angry Birds"
-#cd "/home/maria/.cache/winetricks/dxsdk_jun2010/"
+"PAC-MAN Championship Edition DX+"
 
-cd "/home/maria/.jogos/wineprefixes/Pacman/drive_c/Program Files/PAC-MAN Championship Edition DX+"
-
-/home/maria/.jogos/wines/2.18-x86/bin/wine PAC-MAN.exe
+ PAC-MAN.exe
 
 #winetricks --no-isolate xact_jun2010 wmp10
 
