b8cb05605f73dd6cc8f437d0130f4a6616bc1d0d
Felipe Facundes <felipe.facundes@gmail.com>
2019-07-16 13:37:11 -0300

    Update trine2_start.sh

--- wine-jogos/trine2_start.sh
+++ wine-jogos/trine2_start.sh
@@ -1,4 +1,4 @@
-cd "/home/maria/.jogos/Nativos/trine2_linux/"
+cd "/home/$USER/.jogos/Nativos/trine2_linux/"
 export LD_LIBRARY_PATH="./lib/lib32:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH=./lib/lib32
 ./bin/trine2_linux_32bit
