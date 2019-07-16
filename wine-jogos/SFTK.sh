3740914d6e546a257c4a18355093514eb1851f25
Felipe Facundes <felipe.facundes@gmail.com>
2019-07-16 13:26:00 -0300

    Update SFTK.sh

--- wine-jogos/SFTK.sh
+++ wine-jogos/SFTK.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-export TERM=xterm
-
-export WINE=/home/maria/.jogos/wines/1.9.16/bin/wine64
-export WINEVERPATH=/home/maria/.jogos/wines/1.9.16/bin/wine64
-export LD_LIBRARY32_PATH=/home/maria/.jogos/wines/1.9.16/lib
-export LD_LIBRARY64_PATH=/home/maria/.jogos/wines/1.9.16/lib64
-
-cd "/home/maria/.jogos/wineprefixes/SSFIV/drive_c/Street Fighter X Tekken"
-#cd /home/maria/.cache/winetricks/dxsdk_jun2010
-
-WINEPREFIX=~/.jogos/wineprefixes/SSFIV /home/maria/.jogos/wines/1.9.16/bin/wine64 SFTK.exe
-#DXSDK_Jun10.exe
-
-
+Não te interessa, está tentando ver o que aqui? Vai tomar conta da sua vida.
