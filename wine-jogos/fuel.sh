dac2e56629bfa7d2868ea8b7ac36b410175adeee
Felipe Facundes <felipe.facundes@gmail.com>
2019-07-16 15:33:19 -0300

    Update fuel.sh

--- wine-jogos/fuel.sh
+++ wine-jogos/fuel.sh
@@ -1,27 +1,6 @@
-#!/bin/bash
-#export PATH=/bin/
-#export PATH=/home/maria/.jogos/wines/2.17-x86/bin/
-#export WINE=/home/maria/.jogos/wines/2.17-x86/bin/wine
-#export WINEVERPATH=/home/maria/.jogos/wines/2.17-x86/bin/wine
-#export WINESERVER=/home/maria/.jogos/wines/2.17-x86/bin/wineserver
-#export WINELOADER=/home/maria/.jogos/wines/2.17-x86/bin/wine
-#export WINEDLLPATH=/home/maria/.jogos/wines/2.17-x86/lib
-#export LD_LIBRARY32_PATH=/home/maria/.jogos/wines/2.17-x86/lib
-#export LD_LIBRARY64_PATH=/home/maria/.jogos/wines/2.17-x86/lib64
-export WINEARCH=win32
-export WINEPREFIX=~/.jogos/wineprefixes/fuel/
-#export LIB_ODBC_DRIVER_MANAGER=/usr/lib32/libodbc.so.2.0.0
-#export LIB_ODBC_DRIVER_MANAGER=/usr/lib/libodbc.so.1.0.0
+Não te interessa, está tentando ver o que aqui? Vai tomar conta da sua vida.
 
-#cd "/home/maria/.jogos/wineprefixes/"Mudar o Nome"/drive_c/users/Phillipe/AppData/Roaming/Rovio/Angry Birds Star Wars/updates/Update"
-#cd "/home/maria/.jogos/wineprefixes/"Mudar o Nome"/drive_c/Games/Rovio/Angry Birds"
-#cd "/home/maria/.cache/winetricks/dxsdk_jun2010/"
-cd "/home/maria/.jogos/wineprefixes/fuel/drive_c/Program Files/R.G. Mechanics/Fuel"
-#cd "/home/maria/Downloads/Tudo/Torrents/FUEL/rzr-fuel/Games For Windows Live"
-#cd "/home/maria/.jogos/wineprefixes/fuel/drive_c/Program Files/Microsoft Games for Windows - LIVE/Client"
-#cd ~
-
-wine SecuLauncher.exe
+SecuLauncher.exe
 #msiexec /i wllogin_32.msi 
 #gfwlivesetup.exe GFWLClient.exe GFWLive.exe GameSetup.exe
 #SecuLauncher.exe
