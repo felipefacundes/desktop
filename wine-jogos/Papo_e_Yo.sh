560d86d04386448721b29e3fc127cd34b506586d
Felipe Facundes <felipe.facundes@gmail.com>
2019-07-16 13:22:46 -0300

    Update Papo_e_Yo.sh

--- wine-jogos/Papo_e_Yo.sh
+++ wine-jogos/Papo_e_Yo.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-export PATH=/home/maria/.jogos/wines/2.14/bin/
-export WINE=/home/maria/.jogos/wines/2.14/bin/wine64
-export WINEVERPATH=/home/maria/.jogos/wines/2.14/bin/wine64
-export WINESERVER=/home/maria/.jogos/wines/2.14/bin/wineserver
-export WINELOADER=/home/maria/.jogos/wines/2.14/bin/wine64
-#export WINEDLLPATH=
-export LD_LIBRARY32_PATH=/home/maria/.jogos/wines/2.14/lib
-export LD_LIBRARY64_PATH=/home/maria/.jogos/wines/2.14/lib64
-
-cd "/home/maria/.jogos/wineprefixes/JogosAventura/drive_c/Games/Papo & Yo/Binaries/Win32"
-WINEPREFIX=~/.jogos/wineprefixes/JogosAventura/ /home/maria/.jogos/wines/2.14/bin/wine64 PYGame-Win32-Shipping.exe
+Não te interessa, está tentando ver o que aqui? Vai tomar conta da sua vida.
