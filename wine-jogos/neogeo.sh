0e352bc2555e10644f4f3deb0083e6b02abb6e85
Felipe Facundes <felipe.facundes@gmail.com>
2019-07-16 15:10:42 -0300

    Update neogeo.sh

--- wine-jogos/neogeo.sh
+++ wine-jogos/neogeo.sh
@@ -1,24 +1,8 @@
-#!/bin/bash
-#export PATH=/bin/
-#export PATH=/home/maria/.jogos/wines/2.18-x86/bin/
-export WINE=/home/maria/.jogos/wines/2.18-x86/bin/wine
-export WINEVERPATH=/home/maria/.jogos/wines/2.18-x86/bin/wine
-export WINESERVER=/home/maria/.jogos/wines/2.18-x86/bin/wineserver
-export WINELOADER=/home/maria/.jogos/wines/2.18-x86/bin/wine
-export WINEDLLPATH=/home/maria/.jogos/wines/2.18-x86/lib
-export LD_LIBRARY32_PATH=/home/maria/.jogos/wines/2.18-x86/lib
-export LD_LIBRARY64_PATH=/home/maria/.jogos/wines/2.18-x86/lib64
-export WINEARCH=win32
-export WINEPREFIX=~/.jogos/wineprefixes/neogeo/
-export WINEDEBUG=-all
+Não te interessa, está tentando ver o que aqui? Vai tomar conta da sua vida.
 
-#cd "/home/maria/.jogos/wineprefixes/"Mudar o Nome"/drive_c/users/Phillipe/AppData/Roaming/Rovio/Angry Birds Star Wars/updates/Update"
-#cd "/home/maria/.jogos/wineprefixes/"Mudar o Nome"/drive_c/Games/Rovio/Angry Birds"
-#cd "/home/maria/.cache/winetricks/dxsdk_jun2010/"
+"WinKawaks1.65_NeoFighters_Pack/"
 
-cd "/home/maria/.jogos/wineprefixes/neogeo/drive_c/WinKawaks1.65_NeoFighters_Pack/"
-
-/home/maria/.jogos/wines/2.18-x86/bin/wine WinKawaks.exe
+WinKawaks.exe
 
 #winetricks --no-isolate vcrun6
 #xact_jun2010 wmp10
