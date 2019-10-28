4a1eec591f36b99a29370860b7692faaac9fe3de
Felipe Facundes <felipe.facundes@gmail.com>
2019-10-28 16:57:02 -0300

    Create m4a2mp3.sh

---
+++ scripts/m4a2mp3.sh
@@ -0,0 +1,13 @@
+# Crédito: Felipe Facundes
+# Telegram: @FeFacundes
+for i in *.*;
+  do name=`echo $i | cut -d'.' -f1`;
+  echo $name;
+  ffmpeg -i "$i" -acodec copy -vn -n "${name}.mp3";
+done 
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "Criação de Felipe Facundes"
+echo "Telegram: @FeFacundes"
+echo "Acesse nosso grupo do Telegram:"
+echo "https://t.me/winehq_linux"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
