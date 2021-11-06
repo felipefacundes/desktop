a65ed8e129b4d5c6be4b79f4db6fe3dbd93cd0af
Felipe Facundes <felipe.facundes@gmail.com>
2021-11-06 13:12:57 -0300

    Delete extract-cemu.sh

--- wine-jogos/extract-cemu.sh
+++
@@ -1,9 +0,0 @@
-  mkdir -p "$HOME"/.cemu/wine
-  ln -s /usr/share/cemu/Cemu.exe "$HOME"/.cemu/Cemu.exe
-  ln -s /usr/share/cemu/dbghelp.dll "$HOME"/.cemu/dbghelp.dll
-  ln -s /usr/share/cemu/keystone.dll "$HOME"/.cemu/keystone.dll
-  ln -s /usr/share/cemu/gameProfiles "$HOME"/.cemu/gameProfiles
-  ln -s /usr/share/cemu/graphicPacks "$HOME"/.cemu/graphicPacks
-  cp -r /usr/share/cemu/hfiomlc01 "$HOME"/.cemu/
-  cp -r /usr/share/cemu/mlc01 "$HOME"/.cemu/
-  cp -r /usr/share/cemu/shaderCache "$HOME"/.cemu/
