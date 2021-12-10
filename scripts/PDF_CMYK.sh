53d4c4af8489cae51ded9f36947d4c78522a383c
Felipe Facundes <felipe.facundes@gmail.com>
2021-12-10 18:11:45 -0300

    Update and rename mesclaPDF_CMYK.sh to PDF_CMYK.sh

---
+++ scripts/PDF_CMYK.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# PDF/A
+# -dPDFA=1
+
+# PDF/X-3
+# -dPDFX=true
+
+# Vector / Imagem
+# -sDEVICE=pdfwrite
+
+# Paper size
+# -sPAPERSIZE=a4
+
+# Custom paper size
+# -dDEVICEWIDTHPOINTS=width -dDEVICEHEIGHTPOINTS=height
+
+name=`echo -e "${@}" | cut -d'.' -f1`
+
+gsc -dNOPAUSE -dBATCH -dPDFX=true \
+  -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -dProcessColorModel=/DeviceCMYK \
+  -sColorConversionStrategy=CMYK -sOutputFile="${name}"_CMYK.pdf "${@}"
