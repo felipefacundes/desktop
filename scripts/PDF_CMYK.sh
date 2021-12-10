58323adf917f9d15857365aa430702b2ba06aae8
Felipe Facundes <felipe.facundes@gmail.com>
2021-12-10 19:04:18 -0300

    Update PDF_CMYK.sh

--- scripts/PDF_CMYK.sh
+++ scripts/PDF_CMYK.sh
@@ -6,8 +6,16 @@
 # PDF/X-3
 # -dPDFX=true
 
-# Vector / Image
+# Vector / Imagem
 # -sDEVICE=pdfwrite
+
+# -dPDFSETTINGS=configuration
+#    Presets the "distiller parameters" to one of four predefined settings:
+#        /screen selects low-resolution output similar to the Acrobat Distiller (up to version X) "Screen Optimized" setting.
+#        /ebook selects medium-resolution output similar to the Acrobat Distiller (up to version X) "eBook" setting.
+#        /printer selects output similar to the Acrobat Distiller "Print Optimized" (up to version X) setting.
+#        /prepress selects output similar to Acrobat Distiller "Prepress Optimized" (up to version X) setting.
+#        /default selects output intended to be useful across a wide variety of uses, possibly at the expense of a larger output file.
 
 # Paper size
 # -sPAPERSIZE=a4
