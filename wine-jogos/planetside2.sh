173cdfa3a088b65e4d5c58bd141c5620d8e6c914
Felipe Facundes <felipe.facundes@gmail.com>
2022-08-14 11:25:50 -0300

    Delete planetside2.sh

--- wine-jogos/planetside2.sh
+++
@@ -1,6 +0,0 @@
-Não te interessa, está tentando ver o que aqui? Vai tomar conta da sua vida.
-
-export WINEPREFIX=/home/$USER/.local/share/Steam/steamapps/compatdata/218230/pfx/
-
-PlanetSide2_x64.exe
-#DXSDK_Jun10.exe
