35e40dedc8f2f73d921fefccf593e8b6f7b17697
Felipe Facundes <felipe.facundes@gmail.com>
2022-09-22 19:22:36 -0300

    Update ocr.sh

--- scripts/ocr.sh
+++ scripts/ocr.sh
@@ -1,22 +1,20 @@
-convert *.[jJpP][nNpP][gG] -density 300x300 -quality 100% imagem.pgm
-pdftoppm *.pdf -gray out
+#!/bin/bash
+#pdftoppm *.pdf -gray out
 
-for i in *.pgm;
-do name=`echo $i | cut -d'.' -f1`;
-echo $name;
-convert *.pgm -depth 8 \
--colorspace Gray -type grayscale \
--channel rgba -alpha set -fuzz 50% \
--fill none -opaque white \
--fill white -opaque black \
--fill black -opaque none \
--colors 2 -strip \
--alpha off -negate -threshold 1% -density 300x300 -quality 100% "$i" "${name}.tif";
+for i in *.[jJpP][nNpP][gG]
+  do name=`echo "${i}" | cut -d'.' -f1`
+  echo "${name}"
+  convert "${i}" -density 300x300 -quality 100% "${name}".pgm
+  convert "${name}".pgm -depth 8 \
+  -colorspace Gray -type grayscale \
+  -channel rgba -alpha set -fuzz 50% \
+  -fill none -opaque white \
+  -fill white -opaque black \
+  -fill black -opaque none \
+  -colors 2 -strip \
+  -alpha off -negate -threshold 1% -density 300x300 -quality 100% "${i}" "${name}".tiff
+  rm "${name}".pgm
+  tesseract "${name}.tiff" "${name}" -l por txt
+  rm "${name}".tiff
 done
-
-rm imagem.pgm
-tesseract imagem.tif texto -l por hocr
-echo "Obrigado por converter imagem em texto com o OCR script de Felipe Facundes, o destruidor desde os tempos mais primordios."
-#rm *.tif
 exit
-
